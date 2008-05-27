72e532b50b585d1da95a1340512746cb898b1202
engleek <redeye4@gmail.com>
2008-05-27 20:47:49 +0000

    Implémentation d'une fenêtre OpenGL qui marche, avec des cubes qui servent à rien, mais qui marchent.

--- glwidget.h
+++ glwidget.h
@@ -22,53 +22,32 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
-#include <QGLWidget>
+#include <QtOpenGL>
 
 class GLWidget : public QGLWidget
 {
-    Q_OBJECT
-
+	Q_OBJECT
+	
 public:
-    GLWidget(QWidget *parent = 0);
+    GLWidget(QWidget *parent);
     ~GLWidget();
-
-    QSize minimumSizeHint() const;
-    QSize sizeHint() const;
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+    void timerEvent(QTimerEvent *) { update(); }
+    //void mousePressEvent(QMouseEvent *) { killTimer(timerId); }
+    //void mouseReleaseEvent(QMouseEvent *) { timerId = startTimer(20); }
 
 public slots:
-    void setXRotation(int angle);
-    void setYRotation(int angle);
-    void setZRotation(int angle);
-
-signals:
-    void xRotationChanged(int angle);
-    void yRotationChanged(int angle);
-    void zRotationChanged(int angle);
-
-protected:
-    void initializeGL();
-    void paintGL();
-    void resizeGL(int width, int height);
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
+    void setWorldSpeed(int speed);
 
 private:
-    GLuint makeObject();
-    void quad(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2,
-              GLdouble x3, GLdouble y3, GLdouble x4, GLdouble y4);
-    void extrude(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2);
-    void normalizeAngle(int *angle);
-
-    GLuint object;
-    int xRot;
-    int yRot;
-    int zRot;
-    QPoint lastPos;
-    QColor trolltechGreen;
-    QColor trolltechPurple;
+    int timerId;
+    int worldSpeedFactor;
     
-    GLfloat		rtri;						// Angle For The Triangle ( NEW )
-	GLfloat		rquad;						// Angle For The Quad     ( NEW )
-};
-
-#endif 
+	double gRot;
+	GLint step;
+	GLfloat y1, y2, y3;
+}; 
+ 
+#endif
