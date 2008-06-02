88253150bb34c33cf94b057ef6aada9de35ccca1
engleek <redeye4@gmail.com>
2008-06-02 04:50:54 +0000

    Ajout du dossier interface ( "ça respire plus déja" :)

---
+++ interface/configdialog.cpp
@@ -0,0 +1,98 @@
+/*******************************************************************************************
+ **
+ **    Copyright 2008 Simon & Humphries
+ **
+ **    This file is part of the Reactive Systems Simulator.
+ **
+ **    The Reactive Systems Simulator is free software: you can redistribute it and/or modify
+ **    it under the terms of the GNU General Public License as published by
+ **    the Free Software Foundation, either version 3 of the License, or
+ **    (at your option) any later version.
+ **
+ **    The Reactive Systems Simulator is distributed in the hope that it will be useful,
+ **    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **    GNU General Public License for more details.
+ **
+ **    You should have received a copy of the GNU General Public License
+ **    along with the Reactive Systems Simulator .  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ********************************************************************************************/
+
+#include <QtGui>
+
+#include "configdialog.h"
+#include "pages.h"
+
+ConfigDialog::ConfigDialog()
+{
+	contentsWidget = new QListWidget;
+	contentsWidget->setViewMode(QListView::IconMode);
+	contentsWidget->setIconSize(QSize(84, 73));
+	contentsWidget->setMovement(QListView::Static);
+	contentsWidget->setMinimumHeight(380);
+	contentsWidget->setMaximumWidth(128);
+	contentsWidget->setSpacing(12);
+	
+	pagesWidget = new QStackedWidget;
+	pagesWidget->addWidget(new WorldConfigPage);
+	pagesWidget->addWidget(new ConfigurationPage);
+	pagesWidget->addWidget(new OpenGLPage);
+	
+	QPushButton *closeButton = new QPushButton(tr("Close"));
+	
+	createIcons();
+	contentsWidget->setCurrentRow(0);
+	
+	connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+	
+	QHBoxLayout *horizontalLayout = new QHBoxLayout;
+	horizontalLayout->addWidget(contentsWidget);
+	horizontalLayout->addWidget(pagesWidget, 1);
+	
+	QHBoxLayout *buttonsLayout = new QHBoxLayout;
+	buttonsLayout->addStretch(1);
+	buttonsLayout->addWidget(closeButton);
+	
+	QVBoxLayout *mainLayout = new QVBoxLayout;
+	mainLayout->addLayout(horizontalLayout);
+	mainLayout->addStretch(1);
+	mainLayout->addSpacing(12);
+	mainLayout->addLayout(buttonsLayout);
+	setLayout(mainLayout);
+	
+	setWindowTitle(tr("Config Dialog"));
+}
+
+void ConfigDialog::createIcons()
+{
+	QListWidgetItem *worldConfigButton = new QListWidgetItem(contentsWidget);
+	worldConfigButton->setIcon(QIcon(":/images/browser.png"));
+	worldConfigButton->setText(tr("World\n Configuration"));
+	worldConfigButton->setTextAlignment(Qt::AlignHCenter);
+	worldConfigButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+	
+	QListWidgetItem *programConfigButton = new QListWidgetItem(contentsWidget);
+	programConfigButton->setIcon(QIcon(":/images/systemsettings.png"));
+	programConfigButton->setText(tr("Program\n Configuration"));
+	programConfigButton->setTextAlignment(Qt::AlignHCenter);
+	programConfigButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+	
+	QListWidgetItem *openGLConfigButton = new QListWidgetItem(contentsWidget);
+	openGLConfigButton->setIcon(QIcon(":/images/x.png"));
+	openGLConfigButton->setText(tr("OpenGL\n Configuration"));
+	openGLConfigButton->setTextAlignment(Qt::AlignHCenter);
+	openGLConfigButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+	
+	connect(contentsWidget,
+			SIGNAL(currentItemChanged(QListWidgetItem *, QListWidgetItem *)),
+			this, SLOT(changePage(QListWidgetItem *, QListWidgetItem*)));
+}
+
+void ConfigDialog::changePage(QListWidgetItem *current, QListWidgetItem *previous)
+{
+	if (!current)
+		current = previous;
+	
+	pagesWidget->setCurrentIndex(contentsWidget->row(current));
+}
---
+++ interface/glwidget.cpp
@@ -0,0 +1,204 @@
+/*******************************************************************************************
+**
+**    Copyright 2008 Simon & Humphries
+**
+**    This file is part of the Reactive Systems Simulator.
+**
+**    The Reactive Systems Simulator is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    The Reactive Systems Simulator is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with the Reactive Systems Simulator .  If not, see <http://www.gnu.org/licenses/>.
+**
+********************************************************************************************/
+
+#include "glwidget.h"
+#include <QtGui/QImage>
+
+#include <math.h>
+
+GLWidget::GLWidget(QWidget *parent)
+  : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
+{
+    timerId = startTimer(20);
+    setFormat(QGLFormat(QGL::DoubleBuffer | QGL::DepthBuffer));
+}
+
+GLWidget::~GLWidget()
+{
+}
+
+void GLWidget::mousePressEvent(QMouseEvent *event)
+{
+	QString message = QString("QGLWidget Clicked! (");
+	QString x; x.setNum(event->x());
+	QString y; y.setNum(event->y());
+	message.append(x);
+	message.append(",");
+	message.append(y);
+	message.append(")");
+	emit consoleMsg(message);
+}
+
+
+void GLWidget::setWorldSpeed(int speed)
+{
+	worldSpeedFactor = speed;
+}
+
+void GLWidget::initializeGL()
+{
+    qglClearColor(Qt::black);
+    glShadeModel(GL_FLAT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+	glEnable(GL_TEXTURE_2D);
+	
+	gRot = 0.0;
+	step = 0;
+	y1 = 0.0;
+	y2 = 0.0;
+	y3 = 0.0;
+	worldSpeedFactor = 2;
+	
+	emit consoleMsg(QString("OpenGL load successful..."));
+}
+
+void GLWidget::resizeGL(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    GLfloat x = GLfloat(w) / h;
+    glFrustum(-x, +x, -1.0, +1.0, 4.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void GLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+
+	GLuint cubeTexture = bindTexture(QPixmap(QString(":/images/logo.png")), GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, cubeTexture);
+
+    step += worldSpeedFactor;
+    gRot += 2.0 * (worldSpeedFactor / 2.0);
+    
+	    glLoadIdentity();
+	    glTranslatef(-3.0f, y1, -10.0f);
+		glRotatef(gRot, -0.1f, 1.0f, 0.2f);
+		glBegin(GL_QUADS);
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	    glEnd();    	
+
+	    glLoadIdentity();
+	    glTranslatef(0.5f, y2, -13.0f);
+		glRotatef(gRot, 0.3f, 1.0f, 0.2f);
+		glBegin(GL_QUADS);
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	    glEnd();    	
+
+	    glLoadIdentity();
+	    glTranslatef(5.0f, y3, -16.0f);
+		glRotatef(gRot, 0.7f, -1.0f, -0.1f);
+		glBegin(GL_QUADS);
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+	
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, 1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, 1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, 1.0f, 1.0f);
+
+			glTexCoord2f(1.0f, 0.0f); glVertex3f(1.0f, -1.0f, -1.0f);
+			glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+			glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f, -1.0f, 1.0f);
+			glTexCoord2f(0.0f, 0.0f); glVertex3f(1.0f, -1.0f, 1.0f);
+	    glEnd();    	
+
+	    y1 = qAbs(cos(GLfloat(step) / 30.0))-1.0;
+	    y2 = qAbs(cos(GLfloat(step) / 30.0+20.0))*3.0-2.0;
+	    y3 = qAbs(cos(GLfloat(step) / 30.0+40.0))*6.0-3.0;
+    
+}
