4f6531aeb8b13c80e7357155cab2d268217a712b
iMax-pp <simon.maxime@gmail.com>
2008-09-07 12:12:39 +0000

    luaEngines.h devient luaEngine.h. luaSimpleEngine devient luaEngine tout court. luaMetaEngine disparaît. (pfiout !)
    luaEngine permet de lancer un environnement de lua (un simple constructeur), d'ouvrir un fichier, de lire des paramètres dedans. (regarder dans le workshop pour comment utiliser. :-p)
    Pour ce qui est de lire une méthode instanciée dans un fichier lua, ou de référencer une classe ou une méthode dans lua, ça ne peut pas se faire de manière générale, mais au cas par cas du fait du nombres de paramètres en entrée et du type de la valeur de retour.
    Pour ces choses là on utilisera lunar.

--- lua/luaEngine.h
+++ lua/luaEngine.h
@@ -19,8 +19,8 @@
 **
 ********************************************************************************************/
 
-#ifndef LUAENGINES_H
-#define LUAENGINES_H
+#ifndef LUAENGINE_H
+#define LUAENGINE_H
 
 #include <iostream>
 #include <string>
@@ -28,27 +28,36 @@
 
 using namespace std;
 
-class LuaSimpleEngine
+class LuaEngine
 {
 	public:
-		LuaSimpleEngine();
-		~LuaSimpleEngine();
+		LuaEngine();
+		~LuaEngine();
+		
+		// la fonction pour charger un fichier lua :
 		bool loadFile(const char *fileName);
-		int getInt(const char *varName);
-		float getFloat(const char *varName);
-		const char *getString(const char *varName);
-		bool getBool(const char *varName);
+		
+		// les fonctions pour lire des paramètres dans un fichier lua :
+		int readInt(const char *varName);
+		float readFloat(const char *varName);
+		bool readBool(const char *varName);
+		const char *readString(const char *varName);
+		
+		// la fonction pour lire une fonction dans un fichier lua :
+		// Bon en fait non il n'y aura pas de fonction pour lire une fonction lua.
+		// Le problème pour cela se situe au niveau des paramètres (bah oui le nombre en est variable selon la fonction appelée)
+		// et de la valeur de retour qui est de type inconnu...
 
 	private:
 		lua_State *luaState;
 };
 
-class LuaMetaEngine
+/*class LuaMetaEngine
 {
 	//Très Compliqué (d'ou le nom pourri...), celui ci se charge d'instancier le projet à partir des fichiers projet en Lua.
 	//Meta? Parce que le langage des fichiers projet sera entre C++ et Lua. Les fichiers projet n'étant pas "actifs", ils ne font que configurer les entitées, et tout instancier avant de lancer la simulation.
 	//En quelque sorte, il n'y aura qu'un appel de cette chose par projet...
-};
+};*/
 
 #endif
 
